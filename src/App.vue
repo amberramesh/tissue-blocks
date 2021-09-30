d543a95dae64487596f613502f1d9a80e011eec0
Amber Ramesh <amramesh@iu.edu>
2021-09-30 13:19:01 -0400

    Add pastel color palette

--- src/App.vue
+++ src/App.vue
@@ -29,21 +29,80 @@ const datasets = [
   'KRP462C-1',
   'KRP462P-1'
 ]
-const BASE_URL = 'specimen_data/'
+const colorPalette = [
+  '#db95cd',
+  '#c8feba',
+  '#ddb1f6',
+  '#c2db90',
+  '#f8b8f6',
+  '#99d99d',
+  '#a5a6e4',
+  '#e1e294',
+  '#3bb7ed',
+  '#eda17a',
+  '#6efcfa',
+  '#ffa498',
+  '#2ecdce',
+  '#e395ad',
+  '#98ffdf',
+  '#dd99a2',
+  '#87fff1',
+  '#ffb1b5',
+  '#2ebfb2',
+  '#d3a36a',
+  '#6bb2ed',
+  '#ffe79f',
+  '#8dc5ff',
+  '#c0b46a',
+  '#d5cfff',
+  '#80e2b9',
+  '#cf9fa8',
+  '#beffd4',
+  '#cfa19a',
+  '#87f0ff',
+  '#d2a186',
+  '#42bbcc',
+  '#ffd2a3',
+  '#9ddfff',
+  '#fff5b5',
+  '#b7d4ff',
+  '#e1ffca',
+  '#ffd4e4',
+  '#6ebb96',
+  '#ffd2c1',
+  '#71b6c3',
+  '#c8a580',
+  '#c0fff8',
+  '#c8a491',
+  '#ddffe6',
+  '#bca98f',
+  '#d6e8ff',
+  '#87b78b',
+  '#fdf7ff',
+  '#a3b090',
+  '#fff8ee',
+  '#89b3ba',
+  '#f7ffeb',
+  '#aeaba5',
+  '#9fb0a0'
+];
+const BASE_URL = 'specimen_data/';
 
 export default {
   name: 'App',
   data() {
     return {
-      chart: null
+      chart: null,
+      colorPalette: [...colorPalette]
     }
   },
   methods: {
     getRandomColor() {
-      return `#${parseInt(Math.random() * (Math.pow(16, 6))).toString(16).padStart(6, '0')}`;
+      return this.colorPalette.length ? this.colorPalette.pop() : `#${parseInt(Math.random() * (Math.pow(16, 6))).toString(16).padStart(6, '0')}`;
     }
   },
   async mounted() {
+    this.colorPalette.splice(0, this.length, ...colorPalette);
     const graphData = [];
     let cellTypes = new Set();
     for (const data of datasets) {
